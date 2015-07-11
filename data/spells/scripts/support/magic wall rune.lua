91786e34a2c4799fa36517f06b11ee317c20dded
brunominervino <brunominervino1993@gmail.com>
2015-07-11 18:33:25 -0300

    Spells e Formulas fixadas

--- data/spells/scripts/support/magic wall rune.lua
+++ data/spells/scripts/support/magic wall rune.lua
@@ -1,22 +1,7 @@
-local id = 8753
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
-combat:setParameter(COMBAT_PARAM_CREATEITEM, id)
-
-function removeMw(pos, mw, wall)
-	local tile = Tile(pos)
-	mw = tile:getItemById(mw)
-	wall = tile:getItemById(wall)
-	if mw then mw:remove() end
-	if wall then wall:remove() end
-end
+combat:setParameter(COMBAT_PARAM_CREATEITEM, ITEM_MAGICWALL)
 
 function onCastSpell(creature, var, isHotkey)
-	local c = combat:execute(creature, var)
-	if c then
-		local pos = variantToPosition(var)
-		local wall = Game.createItem(1027, 1, pos)
-		addEvent(removeMw, 20000, pos, id, wall:getId())
-	end
-	return c
+	return combat:execute(creature, var)
 end
